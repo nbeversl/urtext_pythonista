558f6aa8229512726fdab85f127771fb8e9a2646
Nate Beversluis <nathanielbeversluis@gmail.com>
2023-09-16 01:22:07 -0400

    bug fixes; syntax highlighting corrections

--- urtext_pythonista.py
+++ urtext_pythonista.py
@@ -21,7 +21,9 @@ class UrtextEditor(BaseEditor):
 
 	def __init__(self, args):
 		super().__init__(args)
+		self.setup(args)
 
+	def setup(self, args):
 		self.theme = urtext_theme_light # default
 
 		self.urtext_project_path = ''
@@ -39,7 +41,7 @@ class UrtextEditor(BaseEditor):
 			'open_file_to_position' : self.open_file_to_position,
 			'error_message' : self.error_message,
 			'insert_text' : self.insert_text,
-			'save_current' : self.save,
+			'save_current' : self.urtext_save,
 			'set_clipboard' : clipboard.set,
 			'open_external_file' : self.open_in,
 			'open_file_in_editor' : self.open_file,
@@ -49,11 +51,14 @@ class UrtextEditor(BaseEditor):
 			'insert_at_next_line' : self.insert_at_next_line,
 			'popup' : self.popup,
 			'refresh_open_file' : self.refresh_open_file_if_modified,
+			'write_to_console' : print,
 		}
 
 		self._UrtextProjectList = ProjectList(
 			self.urtext_project_path,
 			editor_methods=editor_methods)
+
+		self.download_to_local()
 		self._UrtextProjectList.set_current_project(self.urtext_project_path)
 		self.current_open_file = None
 		self.saved = None
@@ -112,6 +117,13 @@ class UrtextEditor(BaseEditor):
 			self.tv.selected_range, 
 			text)
 
+	def download_to_local(self):
+		url = nsurl('file://' + self.urtext_project_path.replace(' ','%20'))
+		NSFileManager = ObjCClass('NSFileManager').defaultManager()
+		ret = NSFileManager.startDownloadingUbiquitousItemAtURL_error_(
+			url, 
+			None)
+
 	def get_buffer(self):
 		return self.tv.text
 
@@ -141,18 +153,18 @@ class UrtextEditor(BaseEditor):
 	def run_chosen_option(self, function):
 		self.menu_options[function](None)
 
-	def insert_dynamic_def(self,sender):
-		# broken right now
-		node_id = self.new_inline_node(None, locate_inside=False)
+	def insert_dynamic_def(self, sender):
 		position = self.tv.selected_range[0]
-		self.tv.replace_range(self.tv.selected_range, '\n\n[[ ID(>' + node_id + ')\n+( ) +( )\n-( ) -( )\n ]]')
-		self.tv.selected_range = (position + 12, position + 12)
+		self.tv.replace_range(
+			self.tv.selected_range, 
+			'\n\n[[ >( )\n+( ) +( )\n-( ) -( )\n ]]')
+		self.tv.selected_range = (position + 8, position + 8)
 
 	def insert_backtick(self, sender):
 		self.tv.replace_range(self.tv.selected_range, '`')
 	
 	def pop_node(self, sender):
-		self.save()
+		self.urext_save()
 		self._UrtextProjectList.current_project.on_modified(
 			self.current_open_file)
 		file_pos = self.tv.selected_range[0] + 1
@@ -198,6 +210,7 @@ class UrtextEditor(BaseEditor):
 			console.hud_alert('Error happened. Check the Urtext console' ,'error',2)
 
 	def error_message(self, message):
+		console.hud_alert(message, 'error', 5)
 		print(message)
 
 	def reload_projects(self, sender):
@@ -217,27 +230,31 @@ class UrtextEditor(BaseEditor):
 		self._UrtextProjectList.set_current_project(selection)
 
 	def manual_save(self, sender):
-		self.save()
+		self.urtext_save()
 		console.hud_alert('Saved','success',0.5)
 
-	def save(self):
-		if self.saved:
-			return
-		if self.current_open_file:
-			contents = self.tv.text 
-			with open(self.current_open_file,'w', encoding='utf-8') as d:
-				d.write(contents)
+	def urtext_save(self):
+		self.download_to_local()
+		if self.save(None, save_as=False):
+			self.download_to_local()
 			future = self._UrtextProjectList.on_modified(
 				self.current_open_file)
-			self.saved = True
 
 	def open_http_link(self, link):
 		webbrowser.open('safari-'+link)
 	
 	def refresh_open_file_if_modified(self, filename):
 		if filename == self.current_open_file:
-			self.open_file(self.current_open_file, save_first=False)
-				
+			self._UrtextProjectList.current_project.visit_file(
+				filename)
+			self.open_file(
+				self.current_open_file,
+				save_first=False)
+			console.hud_alert(
+				'File contents refreshed',
+				'info',
+				3)
+
 	def refresh_syntax_highlighting(self):
 		position = self.tv.selected_range
 		self.tv.scroll_enabled= False     
@@ -247,33 +264,45 @@ class UrtextEditor(BaseEditor):
 			self.tv.selected_range = position
 		except ValueError:
 			pass
+	
+	def open_file(
+		self, 
+		filename, 
+		save_first=None):
+
+		if save_first == None:
+			save_first = True
+
+		if not os.path.exists(filename):
+			console.hud_alert(
+				'FILE not found. Synced?',
+				'error',
+				1)
+			return None
 		
+		if save_first and self.current_open_file != filename:
+		 	self.urtext_save()
+
+		contents = self.get_file_contents(filename)
+		# prevents issue where the text area is too big
+		# for the new contents:k
+		self.tv.text=''
+		#
+		self.tv.text=contents
+		self.current_open_file_original_contents = contents
+		self.current_open_file = filename
+		self.refresh_syntax_highlighting()
+
 	def open_file_to_position(
 		self,
 		filename, 
-		position):
+		position,
+		node_range=[]):
 
 		self.open_file(filename)
 		self.tv.selected_range = (position, position)
 		self.tvo.scrollRangeToVisible(NSRange(position, 1)) 
-	
-	def open_file(self, filename, save_first=True):
-		if not os.path.exists(filename):
-			console.hud_alert('FILE not found. Synced?','error',1)
-			return None
-		
-		if save_first and self.current_open_file != filename:
-		 	self.save()
 
-		with open(filename,'r', encoding='utf-8') as d:
-			contents=d.read()
-		# prevents issue where the text area is too big
-		# for the new contents:
-		self.tv.text='' 
-		#
-		self.tv.text=contents
-		self.current_open_file = filename
-		self.refresh_syntax_highlighting()
 
 	def timestamp(self, sender):
 		self.tv.replace_range(
@@ -324,13 +353,13 @@ class UrtextEditor(BaseEditor):
 		self.tv.begin_editing()
 
 	def tag_from_other(self, sender):
-		self.save()
+		self.urtext_save()
 		selection = self.tv.selected_range
 		line, cursor = get_full_line(selection[1], self.tv)
-		future = self._UrtextProjectList.current_project.tag_other_node(
+		self._UrtextProjectList.current_project.tag_other_node(
 			line,
-			cursor)
-		#self.refresh_open_file_if_modified(future)
+			cursor,
+			open_files=[self.current_open_file])
 
 	def meta_autocomplete(self, sender): #works	
 		self.autoCompleter.set_items(
